59f280d4dc355010adecd6cba19cdb497a325535
Hersh Nagpal <hershnagpal@gmail.com>
2023-03-20 23:26:21 -0400

    Transitioning from War to ERS

---
+++ ERS/Model/ERSGame.swift
@@ -0,0 +1,8 @@
+//
+//  ERSGame.swift
+//  ERS
+//
+//  Created by Hersh Nagpal on 3/20/23.
+//
+
+import Foundation
---
+++ ERS/View/Pages/ERSView.swift
@@ -0,0 +1,20 @@
+//
+//  ERSView.swift
+//  ERS
+//
+//  Created by Hersh Nagpal on 3/20/23.
+//
+
+import SwiftUI
+
+struct ERSView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ERSView_Previews: PreviewProvider {
+    static var previews: some View {
+        ERSView()
+    }
+}
