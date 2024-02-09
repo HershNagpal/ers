53bf3a05e6340e85c949d8faf53ccbf7ddb1fec7
Hersh Nagpal <hershnagpal@gmail.com>
2024-02-08 20:44:06 -0500

    Game Center authentication added

---
+++ ERS/View/Pages/OnlineGameView.swift
@@ -0,0 +1,18 @@
+//
+//  OnlineGameView.swift
+//  ERS
+//
+//  Created by Hersh Nagpal on 2/8/24.
+//
+
+import SwiftUI
+
+struct OnlineGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnlineGameView()
+}
